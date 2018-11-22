d87febcf76b9c29dcb6c49eedd0d1998ecb85040
crnbrdrck <broderickciaran@gmail.com>
2018-11-22 19:08:01 +0000

    Make a little plan

--- hohoho.py
+++ hohoho.py
@@ -1,2 +1,14 @@
 from whitespace.cli import main
 
+# Take in a file
+
+# Convert `ho` to space
+# Convert `hohoho` to \t
+
+# Write to temp file
+
+# Run temp file with whitespace thingy
+
+# Delete tmp file
+
+# Done
