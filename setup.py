1d970e6c6506119b6abef808c346f816fc4e2a51
crnbrdrck <broderickciaran@gmail.com>
2018-11-22 21:21:30 +0000

    Ignoring dist, made it work

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = 'hohoho',         # How you named your package folder (MyLib)
     packages = ['hohoho'],   # Chose the same as "name"
-    version = '0.1',      # Start with a small number and increase it with every change you make
+    version = '0.2',      # Start with a small number and increase it with every change you make
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description = 'Festive esolang powered by whitespace',   # Give a short description about your library
     author = 'hohoho team',                   # Type in your name
