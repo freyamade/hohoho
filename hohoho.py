ef76b95d6c113f71be8d4210ecf87a8ffe01e5bf
crnbrdrck <broderickciaran@gmail.com>
2018-11-22 19:18:00 +0000

    More plans and stuff

--- hohoho.py
+++ hohoho.py
@@ -1,9 +1,20 @@
+from sys import argv
 from whitespace.cli import main
 
 # Take in a file
+file_name = argv[-1]  # Assume for now it's just the last one
 
+with open(file_name) as input_file:
+    lines = input_file.readlines()
+
+print(lines)
 # Convert `ho` to space
 # Convert `hohoho` to \t
+# Convert \n to \n
+# Read the file line by line
+# Replace hohoho with \t and ho with ' '
+# Replace ' ' with ''
+
 
 # Write to temp file
 
@@ -12,3 +23,4 @@ from whitespace.cli import main
 # Delete tmp file
 
 # Done
+# main([file_name])
